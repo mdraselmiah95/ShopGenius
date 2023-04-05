30199dab6be46c0fe77ee4339bd50dad034e39d6
mdraselmiah95 <russelroy15@gmail.com>
2023-04-06 01:58:31 +0600

    WIP on ShopGenius Banner

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,25 @@
 import "./Banner.scss";
+
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Get lost in your favorite tracks with our noise-canceling
+            headphones. Free shipping on all orders.
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img className="banner-img" src={BannerImg} alt="banner image" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Banner from "./Banner/Banner";
 import "./Home.scss";
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
