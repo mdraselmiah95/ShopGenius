ca2490c11f5d1d06dc422544c58aac31dbbb0d80
mdraselmiah95 <russelroy15@gmail.com>
2023-04-06 00:14:48 +0600

    WIP on ShopGenius Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,19 +8,22 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import PageNotFound from "./components/NotFound/PageNotFound";
+import AppContext from "./utils/context";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category/:id" element={<Category />} />
-        <Route path="/product/:id" element={<SingleProduct />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Newsletter />
-      <Footer />
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
     </BrowserRouter>
   );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
 import "./Header.scss";
+
 const Header = () => {
-    return <div>Header</div>;
+  const [scrolled, setScrolled] = useState(false);
+  const [searchModal, setSearchModal] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <header className="main-header">
+      <div className="header-content">
+        <ul className="left">
+          <li onClick={() => navigate("/")}>Home</li>
+          <li onClick={() => navigate("/about")}>About</li>
+          <li>Categories</li>
+        </ul>
+      </div>
+      <div className="center" onClick={() => navigate("/")}>
+        ShopGenius
+      </div>
+      <div className="right">
+        <TbSearch />
+        <AiOutlineHeart />
+        <span className="cart-icon">
+          <CgShoppingCart />
+        </span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
