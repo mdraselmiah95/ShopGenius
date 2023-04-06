d3c5029d175e1361e7f010d7561a038e91a4f17e
mdraselmiah95 <russelroy15@gmail.com>
2023-04-07 02:43:10 +0600

    WIP on ShopGenius search

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,6 +1,34 @@
+import { MdClose } from "react-icons/md";
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+
+import img from "../../../assets/products/earbuds-prod-2.webp";
+
+const Search = ({ setShowSearch }) => {
+  return (
+    <div className="search-modal">
+      <div className="form-field">
+        <input autoFocus type="text" placeholder="Search for products" />
+        <MdClose className="close-btn" onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="search-result-content">
+        <div className="start-msg">
+          Start typing to see products you are looking for.
+        </div>
+
+        <div className="search-results">
+          <div className="search-result-item">
+            <div className="image-container">
+              <img src={img} alt="image" />
+            </div>
+            <div className="prod-details">
+              <span className="name">title</span>
+              <span className="desc">description</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
