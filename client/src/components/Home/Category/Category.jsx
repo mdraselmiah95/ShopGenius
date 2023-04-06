23d8285521fbc3d5cee1a2c3ef994a69f996b4fb
mdraselmiah95 <russelroy15@gmail.com>
2023-04-06 15:26:02 +0600

    WIP on ShopGenius Category

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,6 +1,25 @@
 import "./Category.scss";
+import cat1 from "../../../assets/category/cat-1.jpg";
+
 const Category = () => {
-    return <div>Category</div>;
+  return (
+    <div className="category-main-content">
+      <div className="categories">
+        <div className="category">
+          <img src={cat1} alt="electronic item" />
+        </div>
+        <div className="category">
+          <img src={cat1} alt="electronic item" />
+        </div>
+        <div className="category">
+          <img src={cat1} alt="electronic item" />
+        </div>
+        <div className="category">
+          <img src={cat1} alt="electronic item" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
+import Category from "./Category/Category";
 import Banner from "./Banner/Banner";
 import "./Home.scss";
 const Home = () => {
   return (
-    <div className="home">
+    <div>
       <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
     </div>
   );
 };
