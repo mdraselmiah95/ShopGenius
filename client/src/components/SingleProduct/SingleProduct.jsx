790ecc017897b0deb433d5f0e6f03bfbeb400e8b
mdraselmiah95 <russelroy15@gmail.com>
2023-04-06 16:56:49 +0600

    WIP on ShopGenius Single Product Discription

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,62 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
+
 import "./SingleProduct.scss";
+import img from "../../assets/products/earbuds-prod-3.webp";
+
 const SingleProduct = () => {
-  return <div>Single Product product</div>;
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img src={img} alt="product" />
+          </div>
+          <div className="right">
+            <span className="name">product.title</span>
+            <span className="price">product.price</span>
+            <span className="desc">product.description</span>
+
+            <div className="cart-buttons">
+              <div className="quantity-buttons">
+                <span>-</span>
+                <span>10</span>
+                <span>+</span>
+              </div>
+              <button className="add-to-cart-button">
+                <FaCartPlus size={20} />
+                ADD TO CART
+              </button>
+            </div>
+
+            <span className="divider" />
+            <div className="info-item">
+              <span className="text-bold">
+                Category:
+                <span>product.categories</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icons">
+                  <FaFacebookF size={16} />
+                  <FaTwitter size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedinIn size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
