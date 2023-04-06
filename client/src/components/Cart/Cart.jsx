423a9e709aaf9263aea1faa529de961d1c894720
mdraselmiah95 <russelroy15@gmail.com>
2023-04-07 00:56:55 +0600

    WIP on ShopGenius side Cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { MdClose } from "react-icons/md";
 import { BsArrowRightCircle, BsCartX } from "react-icons/bs";
 import "./Cart.scss";
+import CartItem from "./CartItem/CartItem";
 
 const Cart = ({ setShowCart }) => {
   return (
@@ -15,6 +16,26 @@ const Cart = ({ setShowCart }) => {
             <BsArrowRightCircle />
           </span>
         </div>
+
+        {/* <div className="empty-cart">
+          <BsCartX />
+          <span>No products in the cart.</span>
+          <button className="return-cta" onClick={() => {}}>
+            RETURN TO SHOP
+          </button>
+        </div> */}
+        <>
+          <CartItem />
+          <div className="cart-footer">
+            <div className="subtotal">
+              <span className="text">Subtotal:</span>
+              <span className="text total">377</span>
+            </div>
+            <div className="button">
+              <button className="checkout-cta">Checkout</button>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   );
