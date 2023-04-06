df9ce33fc6a56531888f286428026640f11b6df6
mdraselmiah95 <russelroy15@gmail.com>
2023-04-06 23:02:07 +0600

    WIP on ShopGenius side Cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,22 @@
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
 import "./Cart.scss";
+
 const Cart = () => {
-    return <div>Cart</div>;
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn">
+            <MdClose className="close-btn" />
+            <span className="text">close</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
 import "./Header.scss";
+import Cart from "../Cart/Cart";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   const [searchModal, setSearchModal] = useState(false);
   const navigate = useNavigate();
 
@@ -24,27 +26,30 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li onClick={() => navigate("/")}>Home</li>
-          <li onClick={() => navigate("/about")}>About</li>
-          <li>Categories</li>
-        </ul>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li onClick={() => navigate("/")}>Home</li>
+            <li onClick={() => navigate("/about")}>About</li>
+            <li>Categories</li>
+          </ul>
 
-        <div className="center" onClick={() => navigate("/")}>
-          ShopGenius
+          <div className="center" onClick={() => navigate("/")}>
+            ShopGenius
+          </div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span className="cart-icon">
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
-        </div>
-      </div>
-    </header>
+      </header>
+      <Cart />
+    </>
   );
 };
 
