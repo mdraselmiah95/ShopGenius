5df79228f4f7062cbea992181d2c655022102c50
mdraselmiah95 <russelroy15@gmail.com>
2023-04-09 02:35:43 +0600

    WIP on ShopGenius Add TO Cart

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,34 +1,60 @@
 import React, { useContext } from "react";
 import { MdClose } from "react-icons/md";
 import "./CartItem.scss";
-import img from "../../../assets/products/earbuds-prod-2.webp";
+import { Context } from "../../../utils/context";
+import Loader from "../../Loader/Loader";
 
 const CartItem = () => {
+  const { cartItems, handleRemoveFromCart, handleCartProductQuantity } =
+    useContext(Context);
+
+  console.log(cartItems);
+
   return (
-    <div className="cart-products">
-      <div className="search-result-item">
-        <div className="image-container">
-          <img src={img} alt="image" />
+    <>
+      {!cartItems ? (
+        <Loader />
+      ) : (
+        <div className="cart-products">
+          {cartItems?.map((item) => (
+            <div className="search-result-item" key={item.id}>
+              <div className="image-container">
+                <img
+                  src={
+                    process.env.VITE_REACT_APP_STRIPE_APP_DEV_URL +
+                    item.attributes.img?.data[0].attributes.url
+                  }
+                />
+              </div>
+              <div className="prod-details">
+                <span className="name">{item.attributes.title}</span>
+                <MdClose
+                  className="close-btn"
+                  onClick={() => handleRemoveFromCart(item)}
+                />
+                <div className="quantity-buttons">
+                  <span onClick={() => handleCartProductQuantity("dec", item)}>
+                    -
+                  </span>
+                  <span>{item.attributes.quantity}</span>
+                  <span onClick={() => handleCartProductQuantity("inc", item)}>
+                    +
+                  </span>
+                </div>
+                <div className="text">
+                  <span>{item.attributes.quantity}</span>
+                  <span>x</span>
+                  <span className="highlight">
+                    <span>&#2547;</span>
+                    {item.attributes.price * item.attributes.quantity}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="prod-details">
-          <span className="name">title</span>
-          <MdClose className="close-btn" />
-          <div className="quantity-buttons">
-            <span>-</span>
-            <span>5</span>
-            <span>+</span>
-          </div>
-          <div className="text">
-            <span>quantity</span>
-            <span>x</span>
-            <span className="highlight">
-              <span>&#8377;</span>
-              price * quantity
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
